7ebdeeb6cfc301e927ae68e4a1f42ba072812da4
Emilio Kariuki <emilio113kariuki@gmail.com>
2022-02-10 19:16:25 +0300

    February 10, 2022 7:16 PM

--- lib/screens/date.dart
+++ lib/screens/date.dart
@@ -31,167 +31,174 @@ class _DateState extends State<Date> {
         title: Text("Kenya Space Agency",
             style: GoogleFonts.robotoCondensed(fontSize: 27, color: kWhite)),
       ),
-      body: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: AnimatedTextKit(
-              animatedTexts: [
-                TypewriterAnimatedText(
-                  'Date',
-                  textStyle: GoogleFonts.robotoCondensed(
-                      fontSize: 27, color: kWhite, fontWeight: FontWeight.w600),
-                  speed: const Duration(milliseconds: 400),
-                ),
-              ],
-              totalRepeatCount: 4,
-              pause: const Duration(milliseconds: 1000),
-              displayFullTextOnTap: true,
-              stopPauseOnTap: true,
+      body: GestureDetector(
+        onTap: (){
+          setState(() {
+            
+          });
+        },
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: AnimatedTextKit(
+                animatedTexts: [
+                  TypewriterAnimatedText(
+                    'Date',
+                    textStyle: GoogleFonts.robotoCondensed(
+                        fontSize: 27, color: kWhite, fontWeight: FontWeight.w600),
+                    speed: const Duration(milliseconds: 400),
+                  ),
+                ],
+                totalRepeatCount: 4,
+                pause: const Duration(milliseconds: 1000),
+                displayFullTextOnTap: true,
+                stopPauseOnTap: true,
+              ),
             ),
-          ),
-          SizedBox(height: 10),
-          Lottie.network(
-            "https://assets7.lottiefiles.com/packages/lf20_ytg7s6tx.json",
-            animate: true,
-            height: size.height * 0.4,
-            width: size.width,
-            fit: BoxFit.fill,
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 20, bottom: 20),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Text("Date Today",
-                    style: GoogleFonts.roboto(fontSize: 24, color: kWhite)),
-              ],
+            SizedBox(height: 10),
+            Lottie.network(
+              "https://assets7.lottiefiles.com/packages/lf20_ytg7s6tx.json",
+              animate: true,
+              height: size.height * 0.4,
+              width: size.width,
+              fit: BoxFit.fill,
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 20, bottom: 5),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                LottieContainer(
-                    func: () {
+            Padding(
+              padding: const EdgeInsets.only(left: 20, bottom: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text("Date Today",
+                      style: GoogleFonts.roboto(fontSize: 24, color: kWhite)),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 20, bottom: 5),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  LottieContainer(
+                      func: () {
+                        setState(() {
+                        showDatePicker(
+                                builder: (context, child) {
+                                  return Theme(
+                                    data: Theme.of(context).copyWith(
+                                      colorScheme: ColorScheme.light(
+                                        primary: Color.fromARGB(255, 201, 22,
+                                            31), // header background color
+                                        onPrimary:
+                                            Colors.black, // header text color
+                                        onSurface: Color.fromARGB(
+                                            255, 0, 0, 0), // body text color
+                                      ),
+                                      textButtonTheme: TextButtonThemeData(
+                                        style: TextButton.styleFrom(
+                                          primary:
+                                              Colors.red, // button text color
+                                        ),
+                                      ),
+                                    ),
+                                    child: child!,
+                                  );
+                                },
+                                context: context,
+                                initialDate: today ?? DateTime.now(),
+                                firstDate: DateTime(2000),
+                                lastDate: DateTime(2023))
+                            .then((value) {
+                          setState(() {
+                            today = value;
+                            today = DateFormat.yMMM().format(today!) as DateTime?;
+                          });
+                        });
+                        });
+                      },
+                      lottieUrl:
+                          "https://assets10.lottiefiles.com/packages/lf20_dAHzdX.json"),
+                  SizedBox(width: size.width * 0.13),
+                  GestureDetector(
+                    onTap: (){
                       setState(() {
-                      showDatePicker(
-                              builder: (context, child) {
-                                return Theme(
-                                  data: Theme.of(context).copyWith(
-                                    colorScheme: ColorScheme.light(
-                                      primary: Color.fromARGB(255, 201, 22,
-                                          31), // header background color
-                                      onPrimary:
-                                          Colors.black, // header text color
-                                      onSurface: Color.fromARGB(
-                                          255, 0, 0, 0), // body text color
-                                    ),
-                                    textButtonTheme: TextButtonThemeData(
-                                      style: TextButton.styleFrom(
-                                        primary:
-                                            Colors.red, // button text color
-                                      ),
-                                    ),
-                                  ),
-                                  child: child!,
-                                );
-                              },
-                              context: context,
-                              initialDate: today ?? DateTime.now(),
-                              firstDate: DateTime(2000),
-                              lastDate: DateTime(2023))
-                          .then((value) {
-                        setState(() {
-                          today = value;
-                          today = DateFormat.yMMM().format(today!) as DateTime?;
-                        });
-                      });
                       });
                     },
-                    lottieUrl:
-                        "https://assets10.lottiefiles.com/packages/lf20_dAHzdX.json"),
-                SizedBox(width: size.width * 0.13),
-                GestureDetector(
-                  onTap: (){
-                    setState(() {
-                    });
-                  },
-                  child: Text(
-                      today == null ? "input a number" : today.toString(),
+                    child: Text(
+                        today == null ? "select date" : today.toString(),
+                        style: GoogleFonts.roboto(fontSize: 24, color: kWhite)),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 5, left: 20, bottom: 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text("Date of Plantation",
                       style: GoogleFonts.roboto(fontSize: 24, color: kWhite)),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 5, left: 20, bottom: 10),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Text("Date of Plantation",
-                    style: GoogleFonts.roboto(fontSize: 24, color: kWhite)),
-              ],
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 20, top: 10),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                LottieContainer(
-                    func: () {
-                      showDatePicker(
-                              builder: (context, child) {
-                                return Theme(
-                                  data: Theme.of(context).copyWith(
-                                    colorScheme: ColorScheme.light(
-                                      primary: Color.fromARGB(255, 201, 22,
-                                          31), // header background color
-                                      onPrimary:
-                                          Colors.black, // header text color
-                                      onSurface: Color.fromARGB(
-                                          255, 0, 0, 0), // body text color
-                                    ),
-                                    textButtonTheme: TextButtonThemeData(
-                                      style: TextButton.styleFrom(
-                                        primary:
-                                            Colors.red, // button text color
+            Padding(
+              padding: const EdgeInsets.only(left: 20, top: 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  LottieContainer(
+                      func: () {
+                        showDatePicker(
+                                builder: (context, child) {
+                                  return Theme(
+                                    data: Theme.of(context).copyWith(
+                                      colorScheme: ColorScheme.light(
+                                        primary: Color.fromARGB(255, 201, 22,
+                                            31), // header background color
+                                        onPrimary:
+                                            Colors.black, // header text color
+                                        onSurface: Color.fromARGB(
+                                            255, 0, 0, 0), // body text color
+                                      ),
+                                      textButtonTheme: TextButtonThemeData(
+                                        style: TextButton.styleFrom(
+                                          primary:
+                                              Colors.red, // button text color
+                                        ),
                                       ),
                                     ),
-                                  ),
-                                  child: child!,
-                                );
-                              },
-                              context: context,
-                              initialDate: plantation ?? DateTime.now(),
-                              firstDate: DateTime(2000),
-                              lastDate: DateTime(2023))
-                          .then((value) {
-                        setState(() {
-                          plantation = value;
-                          plantation = DateFormat.yMMM().format(plantation!) as DateTime?;
+                                    child: child!,
+                                  );
+                                },
+                                context: context,
+                                initialDate: plantation ?? DateTime.now(),
+                                firstDate: DateTime(2000),
+                                lastDate: DateTime(2023))
+                            .then((value) {
+                          setState(() {
+                            plantation = value;
+                            plantation = DateFormat.yMMM().format(plantation!) as DateTime?;
+                          });
                         });
+                      },
+                      lottieUrl:
+                          "https://assets10.lottiefiles.com/packages/lf20_dAHzdX.json"),
+                  SizedBox(width: size.width * 0.13),
+                  GestureDetector(
+                    onTap: (){
+                      setState(() {
                       });
                     },
-                    lottieUrl:
-                        "https://assets10.lottiefiles.com/packages/lf20_dAHzdX.json"),
-                SizedBox(width: size.width * 0.13),
-                GestureDetector(
-                  onTap: (){
-                    setState(() {
-                    });
-                  },
-                  child: Text(
-                      plantation == null ? "input a number" : plantation.toString(),
-                      style: GoogleFonts.roboto(fontSize: 24, color: kWhite)),
-                ),
-              ],
+                    child: Text(
+                        plantation == null ? "Select date" : plantation.toString(),
+                        style: GoogleFonts.roboto(fontSize: 24, color: kWhite)),
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         backgroundColor: Color.fromARGB(255, 189, 139, 31),
