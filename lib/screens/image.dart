e3b88723c8eec640dae219a0adfd11484bd5430c
Emilio Kariuki <emilio113kariuki@gmail.com>
2022-02-10 19:23:16 +0300

    February 10, 2022 7:23 PM

--- lib/screens/image.dart
+++ lib/screens/image.dart
@@ -1,5 +1,7 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
 import "package:flutter/material.dart";
 import 'package:google_fonts/google_fonts.dart';
+import 'package:lottie/lottie.dart';
 import 'package:space/constants.dart/colors.dart';
 
 class Picture extends StatefulWidget {
@@ -12,16 +14,42 @@ class Picture extends StatefulWidget {
 class _PictureState extends State<Picture> {
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Scaffold(
+      backgroundColor: Colors.indigo[400],
       appBar: AppBar(
         backgroundColor: Color.fromARGB(255, 189, 139, 31),
         title: Text("Kenya Space Agency",
             style: GoogleFonts.robotoCondensed(fontSize: 27, color: kWhite)),
-            elevation: 0,
+        elevation: 0,
       ),
       body: Column(
         children: [
-          Text("Image"),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: AnimatedTextKit(
+              animatedTexts: [
+                TypewriterAnimatedText(
+                  'Image',
+                  textStyle: GoogleFonts.robotoCondensed(
+                      fontSize: 27, color: kWhite, fontWeight: FontWeight.w600),
+                  speed: const Duration(milliseconds: 400),
+                ),
+              ],
+              totalRepeatCount: 4,
+              pause: const Duration(milliseconds: 1000),
+              displayFullTextOnTap: true,
+              stopPauseOnTap: true,
+            ),
+          ),
+          SizedBox(height: 10),
+          Lottie.network(
+            "https://assets7.lottiefiles.com/packages/lf20_ytg7s6tx.json",
+            animate: true,
+            height: size.height * 0.4,
+            width: size.width,
+            fit: BoxFit.fill,
+          ),
         ],
       ),
     );
