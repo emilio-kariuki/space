a594f7208e1aabf8d80b9013330d1030d553a169
Emilio Kariuki <emilio113kariuki@gmail.com>
2022-02-10 14:57:12 +0300

    February 10, 2022 2:57 PM

--- lib/constants.dart/colors.dart
+++ lib/constants.dart/colors.dart
@@ -8,3 +8,10 @@ const Color kWhite = Colors.white;
 
 const Color kBack = Color.fromARGB(58, 57, 95, 1);
 
+int _fullAlpha = 255;
+Color blueDark =   Color.fromARGB(_fullAlpha, 51, 92, 129);
+Color blueLight =  Color.fromARGB(_fullAlpha, 116, 179, 206);
+Color yellow =     Color.fromARGB(_fullAlpha, 252, 163, 17);
+Color red =        Color.fromARGB(_fullAlpha, 255, 85, 84);
+Color green =      Color.fromARGB(_fullAlpha, 59, 178, 115);
+
