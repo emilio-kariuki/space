c0aa98604f7bd034f85532422b81335086a87713
Emilio Kariuki <emilio113kariuki@gmail.com>
2022-02-10 13:29:17 +0300

    first commit

---
+++ lib/screens/date.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import "package:google_fonts/google_fonts.dart";
+
+class Date extends StatefulWidget {
+  const Date({Key? key}) : super(key: key);
+
+  @override
+  State<Date> createState() => _DateState();
+}
+
+class _DateState extends State<Date> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Kenya Space Agency",style: GoogleFonts.robotoCondensed(fontSize: 27,)),
+      ),
+    );
+  }
+}
