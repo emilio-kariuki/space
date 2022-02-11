6735852cd6099dff41500be73210416edaa4fee6
Emilio Kariuki <emilio113kariuki@gmail.com>
2022-02-11 17:44:37 +0300

    February 11, 2022 5:44 PM

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:space/screens/date.dart';
+import 'package:space/screens/splash.dart';
 
 void main() {
   runApp(const MaterialApp(
-    home: Date(),
+    home: Splash(),
     debugShowCheckedModeBanner: false,
   ));
 }
--- lib/screens/splash.dart
+++ lib/screens/splash.dart
@@ -1,5 +1,6 @@
 import "package:flutter/material.dart";
 import 'package:lottie/lottie.dart';
+import 'package:space/screens/date.dart';
 
 class Splash extends StatefulWidget {
   const Splash({Key? key}) : super(key: key);
@@ -9,12 +10,21 @@ class Splash extends StatefulWidget {
 }
 
 class _SplashState extends State<Splash> {
+  void initState() {
+    super.initState();
+    Future.delayed(
+        const Duration(seconds: 4),
+        () => Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => Date()),
+            ));
+  }
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
     return Scaffold(
       body: SafeArea(
-        child: Lottie.asset("assets/splash.json")),
+        child: Lottie.asset("assets/splash.json",width: size.width,height: size.height)),
     );
   }
 }
