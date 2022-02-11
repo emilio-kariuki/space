f9c041ccaeafec34b1486fa640af8f05ce158d7f
Emilio Kariuki <emilio113kariuki@gmail.com>
2022-02-11 19:34:52 +0300

    February 11, 2022 7:34 PM

--- lib/screens/splash.dart
+++ lib/screens/splash.dart
@@ -23,7 +23,7 @@ class _SplashState extends State<Splash> {
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
     return Scaffold(
-      backgroundColor: Color.fromARGB(255, 34, 145, 235),
+      backgroundColor: Color.fromARGB(65, 105, 225, 235),
       extendBodyBehindAppBar: true,
       body: Container(width: size.width,height: size.height,child: Lottie.asset("assets/splash.json",width: size.width,height: size.height * 2)),
     );
