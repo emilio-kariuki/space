62dd807680dd89debd328e3ca5f664de20b13ad9
Emilio Kariuki <emilio113kariuki@gmail.com>
2022-02-12 19:34:16 +0300

    February 12, 2022 7:34 PM

--- lib/screens/location.dart
+++ lib/screens/location.dart
@@ -23,15 +23,6 @@ class _LocationState extends State<Location> {
     target: LatLng(37.42796133580664, -122.085749655962),
     zoom: 14.4746,
   );
-
-  static final CameraPosition _kLake = CameraPosition(
-      bearing: 192.8334901395799,
-      target: LatLng(37.43296265331129, -122.08832357078792),
-      tilt: 59.440717697143555,
-      zoom: 19.151926040649414);
-  final CameraPosition initial = CameraPosition(
-      zoom: 15.0,
-        target: LatLng(-0.39244357411624337, 36.958791836761094));
   
   @override
   Widget build(BuildContext context) {
@@ -50,21 +41,16 @@ class _LocationState extends State<Location> {
          GoogleMap(
         mapType: MapType.hybrid,
         initialCameraPosition: _kGooglePlex,
-        onMapCreated: (GoogleMapController controller) {
-          _controller.complete(controller);
-        },
+        
       ),
         ],
       ),
       floatingActionButton: FloatingActionButton.extended(
-        onPressed: _goToTheLake,
+        onPressed: (){},
         label: Text('To the lake!'),
         icon: Icon(Icons.directions_boat),
       ),
     );
   }
-  Future<void> _goToTheLake() async {
-    final GoogleMapController controller = await _controller.future;
-    controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
-  }
+  
 }
