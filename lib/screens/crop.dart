d67e9f310c7062d8e993b9a2e22efcc6766ebb72
Emilio Kariuki <emilio113kariuki@gmail.com>
2022-02-12 14:03:55 +0300

    February 12, 2022 2:03 PM

--- lib/screens/crop.dart
+++ lib/screens/crop.dart
@@ -1,7 +1,8 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
 import "package:flutter/material.dart";
 import 'package:google_fonts/google_fonts.dart';
+import 'package:lottie/lottie.dart';
 import 'package:space/constants.dart/colors.dart';
-
 
 class Crop extends StatefulWidget {
   Crop({Key? key}) : super(key: key);
@@ -13,6 +14,7 @@ class Crop extends StatefulWidget {
 class _CropState extends State<Crop> {
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Scaffold(
       backgroundColor: Colors.indigo[400],
       appBar: AppBar(
@@ -20,6 +22,34 @@ class _CropState extends State<Crop> {
         title: Text("Kenya Space Agency",
             style: GoogleFonts.robotoCondensed(fontSize: 27, color: kWhite)),
         elevation: 0,
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 8),
+            child: AnimatedTextKit(
+              animatedTexts: [
+                TypewriterAnimatedText(
+                  'Image',
+                  textStyle: GoogleFonts.robotoCondensed(
+                      fontSize: 27, color: kWhite, fontWeight: FontWeight.w600),
+                  speed: const Duration(milliseconds: 400),
+                ),
+              ],
+              totalRepeatCount: 4,
+              pause: const Duration(milliseconds: 1000),
+              displayFullTextOnTap: true,
+              // stopPauseOnTap: true,
+            ),
+          ),
+          Lottie.asset(
+            "assets/welcome.json",
+            animate: true,
+            height: size.height * 0.37,
+            width: size.width,
+            fit: BoxFit.fill,
+          ),
+        ],
       ),
     );
   }
