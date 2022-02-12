e10b25f5e3ecd95b3b9c3c844343d7ceb6e97a2f
Emilio Kariuki <emilio113kariuki@gmail.com>
2022-02-12 14:16:25 +0300

    February 12, 2022 2:16 PM

--- lib/screens/crop.dart
+++ lib/screens/crop.dart
@@ -1,4 +1,5 @@
 import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:dropdown_button2/dropdown_button2.dart';
 import "package:flutter/material.dart";
 import 'package:google_fonts/google_fonts.dart';
 import 'package:lottie/lottie.dart';
@@ -13,6 +14,18 @@ class Crop extends StatefulWidget {
 }
 
 class _CropState extends State<Crop> {
+  String? selectedValue;
+  List<String> items = [
+    'Emilio',
+    "Kariuki",
+    "Gichuru",
+    "Jane",
+    "Rosie",
+    "Joy",
+    "Kennedy",
+    "Peter",
+    "Rachel"
+  ];
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
@@ -51,7 +64,66 @@ class _CropState extends State<Crop> {
             fit: BoxFit.fill,
           ),
           SizedBox(height: 5),
-          TextConstant(body: "Plants")
+          TextConstant(body: "Plants"),
+          Row(
+            children: [
+              TextConstant(body: "Type"),
+              
+              DropdownButton2(
+                value: selectedValue,
+                onChanged: (value) {
+                  setState(() {
+                    selectedValue = value as String;
+                    print(selectedValue);
+                  });
+                },
+                icon: const Icon(
+                  Icons.arrow_forward_ios_outlined,
+                ),
+                iconSize: 14,
+                iconEnabledColor: Colors.yellow,
+                iconDisabledColor: Colors.grey,
+                buttonHeight: 50,
+                buttonWidth: 160,
+                items: items
+                    .map((item) => DropdownMenuItem<String>(
+                          value: item,
+                          child: Text(
+                            item,
+                            style: const TextStyle(
+                              fontSize: 14,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black,
+                            ),
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                        ))
+                    .toList(),
+                    buttonPadding: const EdgeInsets.only(left: 14, right: 14),
+                buttonDecoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(2),
+                  border: Border.all(
+                    color: Colors.black26,
+                  ),
+                  color: Color.fromARGB(255, 211, 204, 204),
+                ),
+                buttonElevation: 2,
+                itemHeight: 40,
+                itemPadding: const EdgeInsets.only(left: 14, right: 14),
+                dropdownMaxHeight: 200,
+                dropdownWidth: 200,
+                dropdownPadding: null,
+                dropdownDecoration: BoxDecoration(
+                  color: Color.fromARGB(255, 85, 82, 82),
+                ),
+                dropdownElevation: 8,
+                scrollbarRadius: const Radius.circular(40),
+                scrollbarThickness: 6,
+                scrollbarAlwaysShow: true,
+                offset: const Offset(-20, 0),
+              ),
+            ],
+          )
         ],
       ),
       floatingActionButton: FloatingActionButton(
