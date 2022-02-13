5d192f4bcc8ce5bc6e5569dad3ee7a7aad1f4aba
Emilio Kariuki <emilio113kariuki@gmail.com>
2022-02-13 22:21:30 +0300

    February 13, 2022 10:21 PM

--- lib/screens/date.dart
+++ lib/screens/date.dart
@@ -39,73 +39,7 @@ class _DateState extends State<Date> {
         child: Column(
           mainAxisSize: MainAxisSize.min,
           children: [
-            Stack(
-              children: [
-                Material(
-              elevation: 20,
-                shadowColor: Color.fromARGB(255, 97, 94, 94),
-                shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.only(
-                        bottomLeft: Radius.circular(40),
-                        bottomRight: Radius.circular(40))),
-                child: Container(
-                  height: size.height * 0.15,
-                  width: size.width,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.only(
-                        bottomLeft: Radius.circular(40),
-                        bottomRight: Radius.circular(40)),
-                    color: Colors.white,
-                    shape: BoxShape.rectangle,
-                  ),
-                  child: Column(
-                    children: [
-                       Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.only(left:15,bottom:8,top: 8),
-                            child: BuildBar(
-                              iconUrl: "assets/back.json",
-                              func: () {
-                                Navigator.pop(context);
-                              },
-                            ),
-                          ),
-                          SizedBox(width: 30),
-                          Text("Kenya Space Agency",style: GoogleFonts.redressed(fontSize:25,color: Colors.indigo)),
-                        ],
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: AnimatedTextKit(
-                          animatedTexts: [
-                            WavyAnimatedText(
-                              'Date',
-                              textStyle: GoogleFonts.redressed(
-                                  fontSize: 29,
-                                  color: Colors.indigo,
-                                  fontWeight: FontWeight.w600),
-                              speed: const Duration(milliseconds: 400),
-                            ),
-                          ],
-                          totalRepeatCount: 50,
-                          pause: const Duration(milliseconds: 1000),
-                          displayFullTextOnTap: true,
-                          stopPauseOnTap: true,
-                        ),
-                      ),
-                    ],
-                  ),
-                )),
-                Positioned(
-                  top: 3,
-                  right: 25,
-                  child: Lottie.asset("assets/celebration.json",height:200.1,width: 100.1,animate:true),
-                ),
-              ]
-            ),
-            SizedBox(height: 10),
+             
             Lottie.asset(
               "assets/calendar_date.json",
               animate: true,
