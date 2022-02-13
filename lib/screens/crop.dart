59d4c2f6d26617fe633e97e29fff1bd5007b58c6
Emilio Kariuki <emilio113kariuki@gmail.com>
2022-02-13 13:16:41 +0300

    February 13, 2022 1:16 PM

--- lib/screens/crop.dart
+++ lib/screens/crop.dart
@@ -5,6 +5,7 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:lottie/lottie.dart';
 import 'package:space/constants.dart/Text.dart';
 import 'package:space/constants.dart/colors.dart';
+import 'package:space/screens/image.dart';
 import 'package:space/screens/location.dart';
 
 class Crop extends StatefulWidget {
@@ -220,7 +221,7 @@ class _CropState extends State<Crop> {
         backgroundColor: Color.fromARGB(255, 189, 139, 31),
         onPressed: () {
           Navigator.of(context)
-              .push(MaterialPageRoute(builder: ((context) => Location())));
+              .push(MaterialPageRoute(builder: ((context) => Picture())));
         },
         tooltip: "Next Page",
         child: Center(
