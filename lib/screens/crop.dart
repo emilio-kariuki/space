c97b6351a29d32966ff9f294d5d8a4df10d47fbc
Emilio Kariuki <emilio113kariuki@gmail.com>
2022-02-13 18:58:34 +0300

    February 13, 2022 6:58 PM

--- lib/screens/crop.dart
+++ lib/screens/crop.dart
@@ -5,6 +5,7 @@ import 'package:dropdown_button2/dropdown_button2.dart';
 import "package:flutter/material.dart";
 import 'package:google_fonts/google_fonts.dart';
 import 'package:lottie/lottie.dart';
+import 'package:space/build/build_appBar.dart';
 import 'package:space/constants.dart/colors.dart';
 import 'package:space/screens/date.dart';
 import 'package:space/screens/image.dart';
@@ -60,186 +61,238 @@ class _CropState extends State<Crop> {
     final size = MediaQuery.of(context).size;
     return Scaffold(
      backgroundColor: Color.fromARGB(255, 36, 47, 53),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(top: 8),
-            child: AnimatedTextKit(
-              animatedTexts: [
-                WavyAnimatedText(
-                  'Plant',
-                  textStyle: GoogleFonts.robotoCondensed(
-                      fontSize: 27, color: kWhite, fontWeight: FontWeight.w600),
-                  speed: const Duration(milliseconds: 400),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Stack(
+                children: [
+                  Material(
+                elevation: 20,
+                  shadowColor: Color.fromARGB(255, 97, 94, 94),
+                  shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(40),
+                          bottomRight: Radius.circular(40))),
+                  child: Container(
+                    height: size.height * 0.15,
+                    width: size.width,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(40),
+                          bottomRight: Radius.circular(40)),
+                      color: Colors.white,
+                      shape: BoxShape.rectangle,
+                    ),
+                    child: Column(
+                      children: [
+                         Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.only(left:15,bottom:8,top: 8),
+                              child: BuildBar(
+                                iconUrl: "assets/back.json",
+                                func: () {
+                                  Navigator.pop(context);
+                                },
+                              ),
+                            ),
+                            SizedBox(width: 30),
+                            Text("Kenya Space Agency",style: GoogleFonts.redressed(fontSize:25,color: Colors.indigo)),
+                          ],
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: AnimatedTextKit(
+                            animatedTexts: [
+                              WavyAnimatedText(
+                                'Crop',
+                                textStyle: GoogleFonts.redressed(
+                                    fontSize: 29,
+                                    color: Colors.indigo,
+                                    fontWeight: FontWeight.w600),
+                                speed: const Duration(milliseconds: 400),
+                              ),
+                            ],
+                            totalRepeatCount: 50,
+                            pause: const Duration(milliseconds: 1000),
+                            displayFullTextOnTap: true,
+                            stopPauseOnTap: true,
+                          ),
+                        ),
+                      ],
+                    ),
+                  )),
+                  Positioned(
+                    top: 3,
+                    right: 25,
+                    child: Lottie.asset("assets/celebration.json",height:200.1,width: 100.1,animate:true),
+                  ),
+                ]
+              ),
+              SizedBox(height: 10),
+            Lottie.asset(
+              "assets/plant.json",
+              animate: true,
+              height: size.height * 0.37,
+              width: size.width,
+              fit: BoxFit.fill,
+            ),
+            SizedBox(height: 5),
+            Padding(
+              padding: const EdgeInsets.all(5.0),
+              child: Center(
+                  child: Text("Plants",
+                      style: GoogleFonts.roboto(
+                          fontSize: 26,
+                          color: kWhite,
+                          fontWeight: FontWeight.w600))),
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: Container(
+                      margin: const EdgeInsets.only(left: 15.0, right: 10.0),
+                      child: Divider(
+                        color: Colors.white,
+                        height: 38,
+                      )),
                 ),
               ],
-              totalRepeatCount: 20,
-              pause: const Duration(milliseconds: 1000),
-              displayFullTextOnTap: true,
-              // stopPauseOnTap: true,
             ),
-          ),
-          Lottie.asset(
-            "assets/plant.json",
-            animate: true,
-            height: size.height * 0.37,
-            width: size.width,
-            fit: BoxFit.fill,
-          ),
-          SizedBox(height: 5),
-          Padding(
-            padding: const EdgeInsets.all(5.0),
-            child: Center(
-                child: Text("Plants",
-                    style: GoogleFonts.roboto(
-                        fontSize: 26,
-                        color: kWhite,
-                        fontWeight: FontWeight.w600))),
-          ),
-          Row(
-            children: [
-              Expanded(
-                child: Container(
-                    margin: const EdgeInsets.only(left: 15.0, right: 10.0),
-                    child: Divider(
-                      color: Colors.white,
-                      height: 38,
-                    )),
-              ),
-            ],
-          ),
-          Row(
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(left: 13),
-                child: Text("Type",
-                    style: GoogleFonts.roboto(
-                        fontSize: 26,
-                        color: kWhite,
-                        fontWeight: FontWeight.w600)),
-              ),
-              SizedBox(width: size.width * 0.13),
-              DropdownButton2(
-                value: selectedType,
-                onChanged: (value) {
-                  setState(() {
-                    selectedType = value as String;
-                    print(selectedType);
-                  });
-                },
-                icon: const Icon(
-                  Icons.arrow_forward_ios_outlined,
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 13),
+                  child: Text("Type",
+                      style: GoogleFonts.roboto(
+                          fontSize: 26,
+                          color: kWhite,
+                          fontWeight: FontWeight.w600)),
                 ),
-                iconSize: 18,
-                iconEnabledColor: Color.fromARGB(255, 209, 160, 0),
-                iconDisabledColor: Color.fromARGB(255, 255, 255, 255),
-                buttonHeight: size.height * 0.06,
-                buttonWidth: size.width * 0.65,
-                items: items
-                    .map((item) => DropdownMenuItem<String>(
-                          value: item,
-                          child: Text(
-                            item,
-                            style: GoogleFonts.roboto(
-                                fontSize: 16,
-                                color: Colors.blueGrey[800],
-                                fontWeight: FontWeight.w600),
-                            overflow: TextOverflow.ellipsis,
-                          ),
-                        ))
-                    .toList(),
-                buttonPadding: const EdgeInsets.only(left: 14, right: 14),
-                buttonDecoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  border: Border.all(
-                    color: Color.fromARGB(255, 52, 70, 78),
+                SizedBox(width: size.width * 0.13),
+                DropdownButton2(
+                  value: selectedType,
+                  onChanged: (value) {
+                    setState(() {
+                      selectedType = value as String;
+                      print(selectedType);
+                    });
+                  },
+                  icon: const Icon(
+                    Icons.arrow_forward_ios_outlined,
                   ),
-                  color: Color.fromARGB(255, 255, 255, 255),
-                ),
-                buttonElevation: 0,
-                itemHeight: 40,
-                itemPadding: const EdgeInsets.only(left: 14, right: 14),
-                dropdownMaxHeight: 200,
-                dropdownWidth: 200,
-                dropdownPadding: null,
-                dropdownDecoration: BoxDecoration(
-                  color: Color.fromARGB(255, 236, 234, 234),
-                ),
-                dropdownElevation: 8,
-                scrollbarRadius: const Radius.circular(40),
-                scrollbarThickness: 6,
-                scrollbarAlwaysShow: true,
-                offset: const Offset(-20, 0),
-              ),
-            ],
-          ),
-          SizedBox(height: 10),
-          Row(
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(left: 13),
-                child: Text("Variety",
-                    style: GoogleFonts.roboto(
-                        fontSize: 26,
-                        color: kWhite,
-                        fontWeight: FontWeight.w600)),
-              ),
-              SizedBox(width: size.width * 0.07),
-              DropdownButton2(
-                value: selectedVariety,
-                onChanged: (value) {
-                  setState(() {
-                    selectedVariety = value as String;
-                    print(selectedVariety);
-                  });
-                },
-                icon: const Icon(
-                  Icons.arrow_forward_ios_outlined,
-                ),
-                iconSize: 18,
-                iconEnabledColor: Color.fromARGB(255, 216, 166, 3),
-                iconDisabledColor: Color.fromARGB(255, 255, 255, 255),
-                buttonHeight: size.height * 0.06,
-                buttonWidth: size.width * 0.65,
-                items: itemz
-                    .map((item) => DropdownMenuItem<String>(
-                          value: item,
-                          child: Text(
-                            item,
-                            style: GoogleFonts.roboto(
-                                fontSize: 16,
-                                color: Colors.blueGrey[800],
-                                fontWeight: FontWeight.w600),
-                            overflow: TextOverflow.ellipsis,
-                          ),
-                        ))
-                    .toList(),
-                buttonPadding: const EdgeInsets.only(left: 14, right: 14),
-                buttonDecoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  border: Border.all(
-                    color: Color.fromARGB(255, 52, 70, 78),
+                  iconSize: 18,
+                  iconEnabledColor: Color.fromARGB(255, 209, 160, 0),
+                  iconDisabledColor: Color.fromARGB(255, 255, 255, 255),
+                  buttonHeight: size.height * 0.06,
+                  buttonWidth: size.width * 0.65,
+                  items: items
+                      .map((item) => DropdownMenuItem<String>(
+                            value: item,
+                            child: Text(
+                              item,
+                              style: GoogleFonts.roboto(
+                                  fontSize: 16,
+                                  color: Colors.blueGrey[800],
+                                  fontWeight: FontWeight.w600),
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          ))
+                      .toList(),
+                  buttonPadding: const EdgeInsets.only(left: 14, right: 14),
+                  buttonDecoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    border: Border.all(
+                      color: Color.fromARGB(255, 52, 70, 78),
+                    ),
+                    color: Color.fromARGB(255, 255, 255, 255),
                   ),
-                  color: Color.fromARGB(255, 255, 255, 255),
+                  buttonElevation: 0,
+                  itemHeight: 40,
+                  itemPadding: const EdgeInsets.only(left: 14, right: 14),
+                  dropdownMaxHeight: 200,
+                  dropdownWidth: 200,
+                  dropdownPadding: null,
+                  dropdownDecoration: BoxDecoration(
+                    color: Color.fromARGB(255, 236, 234, 234),
+                  ),
+                  dropdownElevation: 8,
+                  scrollbarRadius: const Radius.circular(40),
+                  scrollbarThickness: 6,
+                  scrollbarAlwaysShow: true,
+                  offset: const Offset(-20, 0),
                 ),
-                buttonElevation: 0,
-                itemHeight: 40,
-                itemPadding: const EdgeInsets.only(left: 14, right: 14),
-                dropdownMaxHeight: 200,
-                dropdownWidth: 200,
-                dropdownPadding: null,
-                dropdownDecoration: BoxDecoration(
-                  color: Color.fromARGB(255, 236, 234, 234),
+              ],
+            ),
+            SizedBox(height: 10),
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 13),
+                  child: Text("Variety",
+                      style: GoogleFonts.roboto(
+                          fontSize: 26,
+                          color: kWhite,
+                          fontWeight: FontWeight.w600)),
                 ),
-                dropdownElevation: 8,
-                scrollbarRadius: const Radius.circular(40),
-                scrollbarThickness: 6,
-                scrollbarAlwaysShow: true,
-                offset: const Offset(-20, 0),
-              ),
-            ],
-          )
-        ],
+                SizedBox(width: size.width * 0.07),
+                DropdownButton2(
+                  value: selectedVariety,
+                  onChanged: (value) {
+                    setState(() {
+                      selectedVariety = value as String;
+                      print(selectedVariety);
+                    });
+                  },
+                  icon: const Icon(
+                    Icons.arrow_forward_ios_outlined,
+                  ),
+                  iconSize: 18,
+                  iconEnabledColor: Color.fromARGB(255, 216, 166, 3),
+                  iconDisabledColor: Color.fromARGB(255, 255, 255, 255),
+                  buttonHeight: size.height * 0.06,
+                  buttonWidth: size.width * 0.65,
+                  items: itemz
+                      .map((item) => DropdownMenuItem<String>(
+                            value: item,
+                            child: Text(
+                              item,
+                              style: GoogleFonts.roboto(
+                                  fontSize: 16,
+                                  color: Colors.blueGrey[800],
+                                  fontWeight: FontWeight.w600),
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          ))
+                      .toList(),
+                  buttonPadding: const EdgeInsets.only(left: 14, right: 14),
+                  buttonDecoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    border: Border.all(
+                      color: Color.fromARGB(255, 52, 70, 78),
+                    ),
+                    color: Color.fromARGB(255, 255, 255, 255),
+                  ),
+                  buttonElevation: 0,
+                  itemHeight: 40,
+                  itemPadding: const EdgeInsets.only(left: 14, right: 14),
+                  dropdownMaxHeight: 200,
+                  dropdownWidth: 200,
+                  dropdownPadding: null,
+                  dropdownDecoration: BoxDecoration(
+                    color: Color.fromARGB(255, 236, 234, 234),
+                  ),
+                  dropdownElevation: 8,
+                  scrollbarRadius: const Radius.circular(40),
+                  scrollbarThickness: 6,
+                  scrollbarAlwaysShow: true,
+                  offset: const Offset(-20, 0),
+                ),
+              ],
+            )
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         backgroundColor: Color.fromARGB(255, 189, 139, 31),
