f9df48bccaf24fee72fad81f7c1dd5d930d1ed14
Emilio Kariuki <emilio113kariuki@gmail.com>
2022-02-13 14:08:56 +0300

    February 13, 2022 2:08 PM

--- lib/screens/submit.dart
+++ lib/screens/submit.dart
@@ -1,10 +1,13 @@
+// ignore_for_file: avoid_print
+
 import "package:flutter/material.dart";
 import 'package:google_fonts/google_fonts.dart';
 import 'package:space/constants.dart/colors.dart';
-
+import 'package:space/screens/location.dart';
 
 class Submit extends StatefulWidget {
-  const Submit({Key? key}) : super(key: key);
+  final User4 value_4;
+  const Submit({Key? key, required this.value_4}) : super(key: key);
 
   @override
   State<Submit> createState() => _SubmitState();
@@ -13,6 +16,13 @@ class Submit extends StatefulWidget {
 class _SubmitState extends State<Submit> {
   @override
   Widget build(BuildContext context) {
+    print(widget.value_4.date_plantation);
+    print(widget.value_4.date_today);
+    print(widget.value_4.type);
+    print(widget.value_4.variety);
+    print(widget.value_4.image);
+    print(widget.value_4.x_coordinate);
+    print(widget.value_4.y_coordinate);
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Color.fromARGB(255, 189, 139, 31),
