bc508163bc8d41feddb4dffcafde7f556eee323a
Emilio Kariuki <emilio113kariuki@gmail.com>
2022-02-13 14:02:30 +0300

    February 13, 2022 2:02 PM

--- lib/screens/crop.dart
+++ lib/screens/crop.dart
@@ -130,7 +130,7 @@ class _CropState extends State<Crop> {
                 onChanged: (value) {
                   setState(() {
                     selectedType = value as String;
-                    print(selectedType);
+                    //print(selectedType);
                   });
                 },
                 icon: const Icon(
@@ -196,7 +196,7 @@ class _CropState extends State<Crop> {
                 onChanged: (value) {
                   setState(() {
                     selectedVariety = value as String;
-                    print(selectedVariety);
+                    // print(selectedVariety);
                   });
                 },
                 icon: const Icon(
