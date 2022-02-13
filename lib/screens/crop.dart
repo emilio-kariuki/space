898a41af943710397e748e012afd382b81c0c078
Emilio Kariuki <emilio113kariuki@gmail.com>
2022-02-13 19:17:10 +0300

    February 13, 2022 7:17 PM

--- lib/screens/crop.dart
+++ lib/screens/crop.dart
@@ -132,7 +132,7 @@ class _CropState extends State<Crop> {
               ),
               SizedBox(height: 10),
             Lottie.asset(
-              "assets/plants.json",
+              "assets/vase.json",
               animate: true,
               height: size.height * 0.37,
               width: size.width,
