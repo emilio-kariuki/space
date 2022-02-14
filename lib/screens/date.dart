acded30fb0c0163e592bf13ba7b2984099bb053a
Emilio Kariuki <emilio113kariuki@gmail.com>
2022-02-14 11:24:34 +0300

    February 14, 2022 11:24 AM

--- lib/screens/date.dart
+++ lib/screens/date.dart
@@ -66,7 +66,7 @@ class _DateState extends State<Date> {
                             Padding(
                               padding: const EdgeInsets.only(left:15,bottom:8,top: 8),
                               child: BuildBar(
-                                iconUrl: "assets/back.json",
+                                iconUrl: "assets/backward.json",
                                 func: () {
                                   Navigator.pop(context);
                                 },
