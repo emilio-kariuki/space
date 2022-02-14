1e967666d95db6d4be95ee05a0060dbf9bb0a1f3
Emilio Kariuki <emilio113kariuki@gmail.com>
2022-02-14 17:49:36 +0300

    February 14, 2022 5:49 PM

--- lib/screens/location.dart
+++ lib/screens/location.dart
@@ -6,6 +6,7 @@ import 'package:flutter/gestures.dart';
 import "package:flutter/material.dart";
 import 'package:google_fonts/google_fonts.dart';
  import 'package:lottie/lottie.dart';
+import 'package:space/build/build_appBar.dart';
 import 'package:space/constants.dart/Text.dart';
 import 'package:space/constants.dart/colors.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
@@ -89,6 +90,90 @@ class _LocationState extends State<Location> {
       body: SafeArea(
         child: Column(
           children: [
+            Stack(
+                  children: [
+                    Material(
+                  elevation: 20,
+                    shadowColor: Color.fromARGB(255, 97, 94, 94),
+                    shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.only(
+                            bottomLeft: Radius.circular(40),
+                            bottomRight: Radius.circular(40))),
+                    child: Container(
+                      height: size.height * 0.15,
+                      width: size.width,
+                      decoration: BoxDecoration(
+                        // border: Border(bottom: BorderSide(color: Colors.blueGrey![800])),
+                        borderRadius: BorderRadius.only(
+                            bottomLeft: Radius.circular(40),
+                            bottomRight: Radius.circular(40)),
+                        color: Colors.white,
+                        shape: BoxShape.rectangle,
+                      ),
+                      child: Column(
+                        children: [
+                           Padding(
+                             padding: const EdgeInsets.only(top: 5),
+                             child: Row(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.only(left:17,bottom:8,top: 8),
+                                  child: BuildBar(
+                                    iconUrl: "assets/backward.json",
+                                    func: () {
+                                      Navigator.pop(context);
+                                    },
+                                  ),
+                                ),
+                                SizedBox(width: 20),
+                                Text("Kenya Space Agency",style: GoogleFonts.redressed(fontSize:25,color: Colors.indigo)),
+                                Padding(
+                                  padding: const EdgeInsets.only(left:20,bottom:8,top: 8),
+                                  child: BuildBar(
+                                    iconUrl: "assets/seetings.json",
+                                    func: () {
+                                      Navigator.pop(context);
+                                    },
+                                  ),
+                                ),
+                              ],
+                          ),
+                           ),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: AnimatedTextKit(
+                              animatedTexts: [
+                                WavyAnimatedText(
+                                  'Date',
+                                  textStyle: GoogleFonts.redressed(
+                                      fontSize: 29,
+                                      color: Colors.indigo,
+                                      fontWeight: FontWeight.w600),
+                                  speed: const Duration(milliseconds: 400),
+                                ),
+                              ],
+                              totalRepeatCount: 100,
+                              pause: const Duration(milliseconds: 1000),
+                              displayFullTextOnTap: true,
+                              stopPauseOnTap: true,
+                            ),
+                          ),
+                        ],
+                      ),
+                    )),
+                    Positioned(
+                      top: 3,
+                      right: 25,
+                      child: Lottie.asset("assets/celebration.json",height:200.1,width: 100.1,animate:true),
+                    ),
+                    Positioned(
+                      top: 3,
+                      left: 25,
+                      child: Lottie.asset("assets/celebration.json",height:200.1,width: 100.1,animate:true),
+                    ),
+                  ]
+                ),
             Lottie.asset(
               "assets/point.json",
               animate: true,
